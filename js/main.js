a435cc052d80703469149c967f799c38ae9390a6
teaguru <teaguru@bk.ru>
2019-05-07 23:54:23 +0400

    completed!

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-alert('234');
+var sum = document.getElementById('button');
+
+sum.addEventListener('click', getN);
+
+function getN() {
+    var getSum = parseFloat(document.getElementById('num_1').value) + parseFloat(document.getElementById('num_2').value);
+    alert( +getSum.toFixed(10) );
+}
