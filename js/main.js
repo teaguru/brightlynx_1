b74f214fd4081d8d168b768bf420196c27cfd527
teaguru <teaguru@bk.ru>
2019-05-08 00:24:18 +0400

    completed!

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var sum = document.getElementById('button');
 
-sum.addEventListener('click', getN);
+sum.addEventListener('click', getN); //создадим обработчик
 
 function getN() {
     var getSum = parseFloat(document.getElementById('num_1').value) + parseFloat(document.getElementById('num_2').value);
-    alert( +getSum.toFixed(10) );
+    alert("Результат" + "\n" + +getSum.toFixed(10) ); //Всё дело в том, что в стандарте IEEE 754 на число выделяется ровно 8 байт(=64 бита), не больше и не меньше.
 }
